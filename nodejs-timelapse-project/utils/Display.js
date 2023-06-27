6dd19144b855371200e00ffc2c07b0879b0d4481
Adam Baker <adb888@usask.ca>
2023-06-27 12:57:07 -0600

    Removed commented out code from the multiple tracks refactor.

--- nodejs-timelapse-project/utils/Display.js
+++ nodejs-timelapse-project/utils/Display.js
@@ -35,11 +35,6 @@ function Display(id, x, y, width, height, padding, scrollbarHeight, frames, time
     ));
     this.mainScrollbarIndex = 0;
 
-    // this.index = 0;
-
-    // this.start = 0;
-    // this.end = this.getSize() - 1;
-
     this.locked = false;
 }
 
@@ -66,94 +61,6 @@ Display.prototype.getIndex = function (scrollbarPos) {
 Display.prototype.getMainScrollbar = function () {
     return this.scrollbars[this.mainScrollbarIndex];
 }
-
-// /* Get the number of segments in the scrollbar */
-// Display.prototype.getSize = function () {
-//     return this.images.length;
-// }
-
-// /* Get the line gap between segments in the scrollbar */
-// Display.prototype.getLineGap = function () {
-//     return this.width / this.getSize();
-// }
-
-// /* Get the x-coordinate of the scrollbar in the canvas */
-// Display.prototype.getScrollbarLeft = function () {
-//     return this.x + this.padding;
-// }
-
-// /* Get the top-most y-coordinate of the scrollbar in the canvas */
-// Display.prototype.getScrollbarTop = function () {
-//     return this.y + this.padding + this.height;
-// }
-
-// /**
-//  * Get the x-coordinate of an index within the scrollbar
-//  * @param {number} index index within the scrollbar
-//  */
-// Display.prototype.getPositionOfIndex = function (index) {
-//     return this.getLineGap() * (0.5 + index) + this.getScrollbarLeft();
-// }
-
-// /* Get the index position x-coordinate of the scrollbar in the canvas */
-// Display.prototype.getMainPosition = function () {
-//     return this.getPositionOfIndex(this.index);
-// }
-
-// /* Get the start position x-coordinate of the scrollbar in the canvas */
-// Display.prototype.getStartPosition = function () {
-//     return this.getPositionOfIndex(this.start);
-// }
-
-// /* Get the end position x-coordinate of the scrollbar in the canvas */
-// Display.prototype.getEndPosition = function () {
-//     return this.getPositionOfIndex(this.end);
-// }
-
-// /**
-//  * Set the index of the display.
-//  * Affects both the scrollbar position and the image being displayed.
-//  * @param {number} index new image index
-//  */
-// Display.prototype.setIndex = function (index) {
-//     if (this.locked) return;
-//     this.index = index;
-//     if (this.index < this.start) {
-//         this.index = this.start;
-//     } else if (this.index > this.end) {
-//         this.index = this.end;
-//     }
-
-//     if (this.index < 0) {
-//         this.index = 0;
-//     } else if (this.index >= this.getSize()) {
-//         this.index = this.getSize() - 1;
-//     }
-// }
-
-// /**
-//  * Set the start position
-//  * @param {number} index new start index
-//  */
-// Display.prototype.setStart = function (index) {
-//     if (this.locked) return;
-//     this.start = index;
-//     if (this.start < 0) this.start = 0;
-//     if (this.start >= this.end) this.start = this.end - 1;
-//     if (this.start > this.index) this.index = this.start;
-// }
-
-// /**
-//  * Set the end position
-//  * @param {number} index new end index
-//  */
-// Display.prototype.setEnd = function (index) {
-//     if (this.locked) return;
-//     this.end = index;
-//     if (this.end >= this.getSize()) this.end = this.getSize() - 1;
-//     if (this.end <= this.start) this.end = this.start + 1;
-//     if (this.end < this.index) this.index = this.end;
-// }
 
 /**
  * Set the lock state of the display.
--- nodejs-timelapse-project/utils/Overlay.js
+++ nodejs-timelapse-project/utils/Overlay.js
@@ -5,8 +5,6 @@ function Overlay(id, x, y, width, height, padding, scrollbarHeight, display, sec
 
     this.images = this.images.slice(display.start, display.end);
     this.secondaryImages = secondaryDisplay.images.slice(secondaryDisplay.start, secondaryDisplay.end);
-    // this.secondarySize = this.secondaryImages.length;
-    // this.secondaryIndex = 0;
 
     this.scrollbars = [];
     let scrollbar1 = new Scrollbar(
@@ -57,77 +55,6 @@ Overlay.prototype.setSecondaryImages = function (images, filter = "") {
     this.scrollbars[1].setSize(this.secondaryImages.length);
     this.secondaryFilter = filter;
 }
-
-// /* Get the number of secondary images in the overlay */
-// Overlay.prototype.getSecondarySize = function () {
-//     return this.secondarySize;
-// }
-
-// /**
-//  * Set the index of the display.
-//  * Affects both the scrollbar position and the images being displayed.
-//  * @param {number} index new image index
-//  */
-// Overlay.prototype.setIndex = function (index) {
-//     let saved = this.index;
-//     if (!this.locked) {
-//         let step = index - saved;
-//         this.index = index;
-//         if (this.index < this.start) {
-//             step = this.start - saved;
-//             this.index = this.start;
-//         } else if (this.index > this.end) {
-//             step = this.end - saved;
-//             this.index = this.end;
-//         }
-
-//         if (this.index < 0) {
-//             step = -saved;
-//             this.index = 0;
-//         } else if (this.index >= this.getSize()) {
-//             step = this.getSize() - 1 - saved;
-//             this.index = this.getSize() - 1;
-//         }
-
-//         let stepRatio = this.getSecondarySize() / this.getSize();
-//         this.secondaryIndex = this.secondaryIndex + (step * stepRatio);
-//         if (this.secondaryIndex < 0) {
-//             this.secondaryIndex = 0;
-//         } else if (this.secondaryIndex >= this.getSecondarySize()) {
-//             this.secondaryIndex = this.getSecondarySize() - 1;
-//         }
-//     }
-// }
-
-// /**
-//  * Set the start position
-//  * @param {number} index new start index
-//  */
-// Overlay.prototype.setStart = function (index) {
-//     if (!this.locked) {
-//         this.start = index;
-//         if (this.start < 0) this.start = 0;
-//         if (this.start >= this.end) this.start = this.end - 1;
-//         if (this.start > this.index) {
-//             this.setIndex(this.start);
-//         }
-//     }
-// }
-
-// /**
-//  * Set the end position
-//  * @param {number} index new end index
-//  */
-// Overlay.prototype.setEnd = function (index) {
-//     if (!this.locked) {
-//         this.end = index;
-//         if (this.end >= this.getSize()) this.end = this.getSize() - 1;
-//         if (this.end <= this.start) this.end = this.start + 1;
-//         if (this.end < this.index) {
-//             this.setIndex(this.end);
-//         }
-//     }
-// }
 
 /**
  * Set the opacity level for the top layer images
