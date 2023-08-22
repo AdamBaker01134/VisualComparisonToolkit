fd07b19282eeab020999ec676a036ba5021c13dd
Adam Baker <adb888@usask.ca>
2023-08-22 12:53:54 -0600

    Did an overhaul on the server to include logging as an endpoint. Now, info, trace, and error messages can be sent to the server and logged in an app.log file.

---
+++ nodejs-timelapse-project/logger.js
@@ -0,0 +1,13 @@
+const pino = require("pino");
+
+module.exports = pino(
+    {
+        formatters: {
+            level: (label) => {
+                return { level: label.toUpperCase() };
+            },
+        },
+        timestamp: pino.stdTimeFunctions.isoTime,
+    },
+    pino.destination(`${__dirname}/app.log`),
+);
--- nodejs-timelapse-project/server.js
+++ nodejs-timelapse-project/server.js
@@ -11,18 +11,17 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
-const cloudinary = require("cloudinary").v2;
+const logger = require("./logger");
 const app = express();
-// const HOST = "localhost";
-const HOST = "hci-sandbox.usask.ca";
 const PORT = 3019;
 
-cloudinary.config({
-    cloud_name: "dvlz4uiyo",
-    api_key: "686265448666564",
-    api_secret: "DIVDhTMDuGGz1McSZhA-VozdKLc",
-    secure: true,
-});
+// const cloudinary = require("cloudinary").v2;
+// cloudinary.config({
+//     cloud_name: "dvlz4uiyo",
+//     api_key: "686265448666564",
+//     api_secret: "DIVDhTMDuGGz1McSZhA-VozdKLc",
+//     secure: true,
+// });
 
 app.use(cors());
 
@@ -35,77 +34,104 @@ app.use(cookieParser());
 app.use(express.static("pages"));
 
 app.get("/", (req, res) => {
-    if (!req.cookies.userId) res.cookie("userId", uuidv4())
+    let userId = req.cookies.userId;
+    if (!userId) {
+        userId = uuidv4();
+        res.cookie("userId", userId);
+        logger.trace({ user_id: userId }, "Generated new user id");
+    }
+    logger.info({ user_id: userId }, "Home page opened");
 	res.sendFile("video-comparison.html", { root: __dirname });
 });
 
 
 app.put("/addSnapshot", (req, res) => {
+    const userId = req.cookies.userId;
     const snapshot = req.body.snapshot;
     fs.readFile("./snapshots.json", (err, json) => {
         let result;
         if (err) {
-            console.log("Error: could not read snapshot json. Generating new json...");
+            logger.error({ user_id: userId }, "Error: could not read snapshot json. Generating new json...");
             result = [];
         } else {
             try {
                 result = JSON.parse(json);
             } catch (err) {
-                console.log("Error: snapshots.json file corrupted. Generating new json...");
+                logger.error({ user_id: userId }, "Error: snapshots.json file corrupted. Generating new json...");
                 result = [];
             }
         }
         result.push(snapshot);
         fs.writeFile("./snapshots.json", JSON.stringify(result), err => {
             if (err) {
-                console.error("Error: could not write updated json to disk...");
-                res.status(400);
-                res.send(err);
+                logger.error({ user_id: userId }, "Error: could not write updated json to disk...");
+                res.status(400).send(err);
             } else {
-                res.status(200);
-                res.send("Ok.");
+                logger.trace({ user_id: userId }, "New snapshot successfully added to disk.");
+                res.status(200).send("Ok.");
             }
         });
     });
 });
 
-app.get("/getImages", (req, res) => {
-	let dataset = req.query.dataset;
-	let result = [];
-
-    let options = { resource_type: "image", folder: dataset, max_results: 1000 };
-
-    const listResources = (next_cursor) => {
-        if (next_cursor) {
-            options["next_cursor"] = next_cursor;
+app.post("/log", (req, res) => {
+    const userId = req.cookies.userId;
+    try {
+        switch (req.body.type) {
+            case "trace":
+                logger.trace({ user_id: userId }, req.body.msg);
+                break;
+            case "error":
+                logger.error({ user_id: userId }, req.body.msg);
+                break;
+            case "info":
+            default:
+                logger.info({ user_id: userId }, req.body.msg);
+                break;
         }
-        // console.log(options);
-        cloudinary.api.resources(options, (error, cloudinary_res) => {
-            if (error) {
-                console.error(error);
-            }
-            let more = cloudinary_res.next_cursor;
-            let resources = cloudinary_res.resources;
-
-            for (let resource in resources) {
-                let resource_object = resources[resource];
-                let url = resource_object.secure_url;
-				if (url.includes(dataset)) {
-					result.push(url);
-				}
-            }
-
-            if (more) {
-                listResources(more);
-            } else {
-                // console.log("done");
-				res.send(result);
-            }
-        })
+        res.status(200).send("Logged successfully.");
+    } catch (err) {
+        res.status(400).send(err);
     }
-	listResources(null);
 });
 
+// app.get("/getImages", (req, res) => {
+// 	let dataset = req.query.dataset;
+// 	let result = [];
+
+//     let options = { resource_type: "image", folder: dataset, max_results: 1000 };
+
+//     const listResources = (next_cursor) => {
+//         if (next_cursor) {
+//             options["next_cursor"] = next_cursor;
+//         }
+//         // console.log(options);
+//         cloudinary.api.resources(options, (error, cloudinary_res) => {
+//             if (error) {
+//                 console.error(error);
+//             }
+//             let more = cloudinary_res.next_cursor;
+//             let resources = cloudinary_res.resources;
+
+//             for (let resource in resources) {
+//                 let resource_object = resources[resource];
+//                 let url = resource_object.secure_url;
+// 				if (url.includes(dataset)) {
+// 					result.push(url);
+// 				}
+//             }
+
+//             if (more) {
+//                 listResources(more);
+//             } else {
+//                 // console.log("done");
+// 				res.send(result);
+//             }
+//         })
+//     }
+// 	listResources(null);
+// });
+
 app.listen(PORT, () => {
-	console.log(`Successfully started express node js server. Listening on http://${HOST}:${PORT}.`);
+    logger.info("Successfully started express node js server.");
 });
